3a1eeaf3dc4d358620d2a8037c4c8275ccef9006
Priyanka Perera <pcperera@gmail.com>
2023-08-01 11:23:13 +0530

    Incrase hidden layers to 512

--- src/agent.py
+++ src/agent.py
@@ -17,7 +17,7 @@ class Agent:
         self.epsilon = 0 # randomness
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = Linear_QNet(11, 256, 3)
+        self.model = Linear_QNet(11, 1024, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
 
